6c1528688687fd7ebc1ca48e1350af2a80cdb519
Peng Gu <penggu@gmail.com>
2022-05-05 03:00:58 -0700

    Add script to do Windows backup before update

---
+++ windows/auto_update/windows_update/04_02_create_windows_restore_point.bat
@@ -0,0 +1,30 @@
+REM This script need to run in an elevated window.
+
+
+REM Step 1: allow backup to happen less than 1440 minutes.
+REM by edit Windows Registry
+
+REM Method 1: import reg file using regedit (worked)
+REM regedit.exe /s 04_01_change_windows_restore_frequency.reg
+
+REM Method 2: add value/data under a key directly using reg (worked)
+Reg Add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion\SystemRestore" /v SystemRestorePointCreationFrequency /t REG_DWORD /d 2 /f
+
+GOTO comment
+Method 3: Use powershell (not verified)
+
+# Create new items with values
+New-ItemProperty -Path 'HKCU:\Software\NewTestKey' -Name  'TestValue' -Value '1' -PropertyType 'DWORD' –Force
+# Get out of the Registry
+Pop-Location
+:comment
+
+
+
+REM Step 2: Create Windows Restore Point
+
+REM Method 1: Use PowerShell (worked)
+REM PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -NoExit -Command "Checkpoint-Computer -Description "(Get-Date) Before Windows Update" -RestorePointType "MODIFY_SETTINGS"; " ' " -Verb RunAs}"
+
+REM Method 2: Use wmic (worked)
+Wmic.exe /Namespace:\\root\default Path SystemRestore Call CreateRestorePoint "%DATE% %TIME% Before Windows Update", 100, 12
